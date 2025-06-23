f0d86ce4ee0f5455a488600e20839ec07b261e9f
Pradhuman Singh Naruka <145077788+PradhumanSinghNaruka@users.noreply.github.com>
2025-06-23 18:31:21 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -23,7 +23,7 @@ function Cards() {
   className="block text-center p-4 bg-black text-white cursor-pointer border border-white rounded-2xl w-[300px] ml-3 md:ml-0 space-y-6 hover:bg-white hover:text-black hover:border-black duration-300 shadow-2xl shadow-black"
   onClick={() => window.open("https://psn-shoes.vercel.app/", "_blank")}
 >
-  <CgWebsite className="w-10 h-10 text-center ml-24 md:ml-24 mt-4" />
+  <CgWebsite className="w-10 h-10 text-center ml-24 md:ml-28 mt-4" />
   <h1 className="text-3xl font-semiblock">Shoes Full Stack Project</h1>
   <p className="text-clip text-wrap">
     Developed a full stack Shoes-Store Website. Provide a user-friendly platform for purchasing branded footwear. 07/2023 – 11/2023
@@ -35,7 +35,7 @@ function Cards() {
   className="block text-center p-4 bg-black text-white cursor-pointer border rounded-xl w-[300px] ml-3 md:ml-0 space-y-6 hover:bg-white hover:text-black hover:border-black duration-300 shadow-2xl shadow-black"
   onClick={() => window.open("https://neftap.com/", "_blank")}
 >
-  <CgWebsite className="w-10 h-10 text-center ml-24 md:ml-24 mt-4" />
+  <CgWebsite className="w-10 h-10 text-center ml-24 md:ml-28 mt-4" />
   <h1 className="text-3xl font-semiblock">NefTap Internship Project</h1>
   <p className="text-clip text-wrap">
     Developed a full stack NefTap Website. Provide a user-friendly platform for purchasing NFC cards. In this Project we use React, Express, My SQL, MongoDB, Node.js 01/05/2025 – 15/07/2025
@@ -47,7 +47,7 @@ function Cards() {
   className="block text-center p-4 bg-black text-white cursor-pointer border rounded-xl w-[300px] ml-3 md:ml-0 space-y-6 hover:bg-white hover:text-black hover:border-black duration-300 shadow-2xl shadow-black"
   onClick={() => window.open("https://portfolio-pradhuman-singh.vercel.app/", "_blank")}
 >
-  <CgWebsite className="w-10 h-10 text-center ml-24 md:ml-24 mt-4" />
+  <CgWebsite className="w-10 h-10 text-center ml-24 md:ml-28 mt-4" />
   <h1 className="text-3xl font-semiblock">Portfolio Full Stack Project</h1>
   <p className="text-clip text-wrap">
    Built a Full Stack Portfolio Website showcasing my projects, skills, and contact information with a user-friendly interface. 01/2024 – 11/2024
